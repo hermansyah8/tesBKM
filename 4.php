cd499bc2919841b19009e97c0105a0e189ff884d
hermansyah8 <74259029+hermansyah8@users.noreply.github.com>
2020-11-11 15:40:15 +0700

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,17 @@
+<?php 
+	$a = 'Aku suka makan nasi padang';
+	echo substr($a,0,-22);
+	echo "<br>";
+	echo substr($a,0,-18);
+	echo "<br>";
+	echo substr($a,0,-11);
+	echo "<br>";
+	echo substr($a,0,-6);
+	echo "<br>";
+	echo substr($a,0);
+	echo "<br>";
+	echo substr($a,-6);
+	echo "<br>";
+	echo substr($a,-11,6);
+
+ ?>
